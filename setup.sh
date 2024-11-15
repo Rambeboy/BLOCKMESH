e983b8841854cb8657232e406a3607e1cf1c3223
R4MB3B0Y <kingindoslayer@gmail.com>
2024-11-15 14:51:28 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -6,7 +6,7 @@ GREENCOLOR="\e[32m"
 DEFAULTCOLOR="\e[0m"
 
 setup() {
-    curl -s https://raw.githubusercontent.com/Rambeboy/Rambeboy/refs/heads/main/ShowLogo.sh | bash
+    curl -s https://raw.githubusercontent.com/Rambeboy/Rambeboy/refs/heads/main/BlockLogo.sh | bash
     sleep 3
 
     echo "Updating & Upgrading Packages..."
